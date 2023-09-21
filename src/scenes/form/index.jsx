eae368e72971fdb50b8d1a929b5800b2e510cc7e
Sankalp Patel <56015507+sankalp918@users.noreply.github.com>
2023-09-21 19:28:11 +0000

    Validation Fix Validation Fix

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -133,12 +133,12 @@ const phoneRegExp =
     /^(?:\+\d{1,4}\s*)?\d{10}$/;
 
 const userSchema = yup.object().shape({
-    firstName: yup.string().required("required").minLength(3).maxLength(20),
-    lastName: yup.string().required("required").minLength(3).maxLength(20),
+    firstName: yup.string().required("required"),
+    lastName: yup.string().required("required"),
     email: yup.string().email("invalid email").required("required"),
     contact: yup.string().matches(phoneRegExp, "Phone number is not valid").required("required"),
-    address1: yup.string().required("required").minLength(10).maxLength(50),
-    address2: yup.string().required("required").minLength(10).maxLength(50)
+    address1: yup.string().required("required"),
+    address2: yup.string().required("required")
 });
 const initialValues = {
     firstName: "",
